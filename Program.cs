c2d825ed6be3684e7f7124a8aedd281c8254a3c6
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-05-28 09:42:52 -0700

    - Visual studio wtf did you do to my formatting?

--- Program.cs
+++ Program.cs
@@ -8,197 +8,197 @@ namespace DrakiaXYZ_MapInfoExtractor
 {
     internal class Program
     {
-		private static Dictionary<string, int> _sceneLookup = new Dictionary<string, int>();
-		private static Dictionary<string, dynamic> _presetCache = new Dictionary<string, dynamic>();
+        private static Dictionary<string, int> _sceneLookup = new Dictionary<string, int>();
+        private static Dictionary<string, dynamic> _presetCache = new Dictionary<string, dynamic>();
 
-		static void Main(string[] args)
-		{
-			// First, extract "globalmanagers/BuildSettings" from "EscapeFromTarkov_Data\globalgamemanagers" into "Extract" (This should end up in a folder called "globalgamemanagers")
-			// Second, extract all of the map preset bundles from "EscapeFromTarkov_Data\StreamingAssets\Windows\maps" into "Extract". The script will look for the first folder containing ".bundle" in its name
-			string outputDir = ".\\Extract";
-			if (!buildSceneLookup(outputDir))
+        static void Main(string[] args)
+        {
+            // First, extract "globalmanagers/BuildSettings" from "EscapeFromTarkov_Data\globalgamemanagers" into "Extract" (This should end up in a folder called "globalgamemanagers")
+            // Second, extract all of the map preset bundles from "EscapeFromTarkov_Data\StreamingAssets\Windows\maps" into "Extract". The script will look for the first folder containing ".bundle" in its name
+            string outputDir = ".\\Extract";
+            if (!buildSceneLookup(outputDir))
             {
-				return;
+                return;
             }
 
-			// Find the bundle dir, and parse it
-			foreach (string dir in Directory.GetDirectories(outputDir))
-			{
-				if (!dir.Contains(".bundle")) continue;
-
-				if (!parseBundles(dir))
-				{
-					return;
-				}
-
-				break;
-			}
-		}
-
-		static bool buildSceneLookup(string outputDir)
-        {
-			// Create a mapping of scene -> id from ExportedProject\ProjectSettings\EditorBuildSettings.asset
-			string buildSettingsPath = $"{outputDir}\\globalgamemanagers\\ExportedProject\\ProjectSettings\\EditorBuildSettings.asset";
-			dynamic? buildSettings = loadYamlAsDynamic(buildSettingsPath);
-			if (buildSettings == null)
-			{
-				Console.Error.WriteLine("Error loading EditorBuildSettings.asset");
-				return false;
-			}
-
-			for (int i = 0; i < buildSettings.EditorBuildSettings.m_Scenes.Count; i++)
-			{
-				dynamic scene = buildSettings.EditorBuildSettings.m_Scenes[i];
-				_sceneLookup.Add(scene.path, i);
-			}
-
-			return true;
-		}
-
-		static bool parseBundles(string bundleDir)
-        {
-			if (!buildPresetCache(bundleDir))
+            // Find the bundle dir, and parse it
+            foreach (string dir in Directory.GetDirectories(outputDir))
             {
-				return false;
-            }
+                if (!dir.Contains(".bundle")) continue;
 
-			string presetDir = $"{bundleDir}\\ExportedProject\\Assets\\Content\\Locations\\_Presets";
-			List<string> assetList;
-			if (!parsePresetDir(presetDir, out assetList))
-            {
-				return false;
-            }
-
-			return true;
-        }
-
-		static bool buildPresetCache(string bundleDir)
-        {
-			string monoBehaviourDir = $"{bundleDir}\\ExportedProject\\Assets\\MonoBehaviour";
-			foreach (string file in Directory.GetFiles(monoBehaviourDir))
-            {
-				if (!file.EndsWith(".asset")) continue;
-				dynamic? meta = loadYamlAsDynamic(file + ".meta");
-				dynamic? preset = loadYamlAsDynamic(file);
-				if (meta == null || preset == null)
+                if (!parseBundles(dir))
                 {
-					Console.Error.WriteLine("Error reading meta or preset " + file);
-					continue;
+                    return;
                 }
 
-				_presetCache.Add(meta.guid, preset);
-			}
-
-			return true;
+                break;
+            }
         }
 
-		static bool parsePresetDir(string assetDir, out List<string> assetList)
+        static bool buildSceneLookup(string outputDir)
         {
-			assetList = new List<string>();
-
-			foreach (string file in Directory.GetFiles(assetDir))
+            // Create a mapping of scene -> id from ExportedProject\ProjectSettings\EditorBuildSettings.asset
+            string buildSettingsPath = $"{outputDir}\\globalgamemanagers\\ExportedProject\\ProjectSettings\\EditorBuildSettings.asset";
+            dynamic? buildSettings = loadYamlAsDynamic(buildSettingsPath);
+            if (buildSettings == null)
             {
-				if (!file.EndsWith(".asset")) continue;
+                Console.Error.WriteLine("Error loading EditorBuildSettings.asset");
+                return false;
+            }
 
-				dynamic? preset = loadYamlAsDynamic(file);
-				if (preset == null)
-				{
-					Console.Error.WriteLine("Error loading preset " + file);
-					continue;
-				}
+            for (int i = 0; i < buildSettings.EditorBuildSettings.m_Scenes.Count; i++)
+            {
+                dynamic scene = buildSettings.EditorBuildSettings.m_Scenes[i];
+                _sceneLookup.Add(scene.path, i);
+            }
 
-				assetList = new List<string>();
-				processPreset(preset, ref assetList);
+            return true;
+        }
 
-				if (preset.MonoBehaviour.ServerName != "")
-				{
-					Dictionary<int, string> assetMap = new Dictionary<int, string>();
-					Console.WriteLine($"{preset.MonoBehaviour.ServerName} consists of the following assets:");
-					foreach (string asset in assetList)
+        static bool parseBundles(string bundleDir)
+        {
+            if (!buildPresetCache(bundleDir))
+            {
+                return false;
+            }
+
+            string presetDir = $"{bundleDir}\\ExportedProject\\Assets\\Content\\Locations\\_Presets";
+            List<string> assetList;
+            if (!parsePresetDir(presetDir, out assetList))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool buildPresetCache(string bundleDir)
+        {
+            string monoBehaviourDir = $"{bundleDir}\\ExportedProject\\Assets\\MonoBehaviour";
+            foreach (string file in Directory.GetFiles(monoBehaviourDir))
+            {
+                if (!file.EndsWith(".asset")) continue;
+                dynamic? meta = loadYamlAsDynamic(file + ".meta");
+                dynamic? preset = loadYamlAsDynamic(file);
+                if (meta == null || preset == null)
+                {
+                    Console.Error.WriteLine("Error reading meta or preset " + file);
+                    continue;
+                }
+
+                _presetCache.Add(meta.guid, preset);
+            }
+
+            return true;
+        }
+
+        static bool parsePresetDir(string assetDir, out List<string> assetList)
+        {
+            assetList = new List<string>();
+
+            foreach (string file in Directory.GetFiles(assetDir))
+            {
+                if (!file.EndsWith(".asset")) continue;
+
+                dynamic? preset = loadYamlAsDynamic(file);
+                if (preset == null)
+                {
+                    Console.Error.WriteLine("Error loading preset " + file);
+                    continue;
+                }
+
+                assetList = new List<string>();
+                processPreset(preset, ref assetList);
+
+                if (preset.MonoBehaviour.ServerName != "")
+                {
+                    Dictionary<int, string> assetMap = new Dictionary<int, string>();
+                    Console.WriteLine($"{preset.MonoBehaviour.ServerName} consists of the following assets:");
+                    foreach (string asset in assetList)
                     {
-						if (!_sceneLookup.ContainsKey(asset))
-						{
-							Console.WriteLine($"    {asset}");
-						}
-						else
-						{
-							int levelNumber = _sceneLookup[asset];
-							if (!assetMap.ContainsKey(levelNumber))
+                        if (!_sceneLookup.ContainsKey(asset))
+                        {
+                            Console.WriteLine($"    {asset}");
+                        }
+                        else
+                        {
+                            int levelNumber = _sceneLookup[asset];
+                            if (!assetMap.ContainsKey(levelNumber))
                             {
-								assetMap.Add(levelNumber, asset);
+                                assetMap.Add(levelNumber, asset);
                             }
-						}
+                        }
                     }
 
-					foreach (int levelNumber in assetMap.Keys.OrderBy(x => x))
+                    foreach (int levelNumber in assetMap.Keys.OrderBy(x => x))
                     {
-						Console.WriteLine($"    level{levelNumber}  ({assetMap[levelNumber]})");
-					}
-				}
-			}
-
-			foreach (string folder in Directory.GetDirectories(assetDir))
-            {
-				List<string> childAssets;
-				if (!parsePresetDir(folder, out childAssets))
-                {
-					return false;
+                        Console.WriteLine($"    level{levelNumber}  ({assetMap[levelNumber]})");
+                    }
                 }
-
-				assetList.AddRange(childAssets);
             }
 
-			return true;
+            foreach (string folder in Directory.GetDirectories(assetDir))
+            {
+                List<string> childAssets;
+                if (!parsePresetDir(folder, out childAssets))
+                {
+                    return false;
+                }
+
+                assetList.AddRange(childAssets);
+            }
+
+            return true;
         }
 
-		static void processPreset(dynamic preset, ref List<string> assetList)
+        static void processPreset(dynamic preset, ref List<string> assetList)
         {
-			foreach (dynamic scene in preset.MonoBehaviour._scenesResourceKeys)
-			{
-				assetList.Add(scene.path);
-			}
-
-			foreach (dynamic childPreset in preset.MonoBehaviour.ChildPresets)
+            foreach (dynamic scene in preset.MonoBehaviour._scenesResourceKeys)
             {
-				if (_presetCache.ContainsKey(childPreset.guid))
+                assetList.Add(scene.path);
+            }
+
+            foreach (dynamic childPreset in preset.MonoBehaviour.ChildPresets)
+            {
+                if (_presetCache.ContainsKey(childPreset.guid))
                 {
-					processPreset(_presetCache[childPreset.guid], ref assetList);
+                    processPreset(_presetCache[childPreset.guid], ref assetList);
                 }
             }
-		}
+        }
 
-		static dynamic? loadYamlAsDynamic(string yamlPath)
+        static dynamic? loadYamlAsDynamic(string yamlPath)
         {
-			// Hacky workaround to bypass tags in the YAML
-			var inputStringBuilder = new StringBuilder();
-			using (var inputReader = new StreamReader(yamlPath))
-			{
-				bool bStartReading = yamlPath.EndsWith(".meta");
-				string? line;
-				while ((line = inputReader.ReadLine()) != null)
-				{
-					if (!bStartReading)
+            // Hacky workaround to bypass tags in the YAML
+            var inputStringBuilder = new StringBuilder();
+            using (var inputReader = new StreamReader(yamlPath))
+            {
+                bool bStartReading = yamlPath.EndsWith(".meta");
+                string? line;
+                while ((line = inputReader.ReadLine()) != null)
+                {
+                    if (!bStartReading)
                     {
-						if (line.StartsWith("---")) bStartReading = true;
-						continue;
+                        if (line.StartsWith("---")) bStartReading = true;
+                        continue;
                     }
 
-					inputStringBuilder.AppendLine(line);
-				}
-			}
-			var inputString = inputStringBuilder.ToString();
-			var inputStringReader = new StringReader(inputString);
+                    inputStringBuilder.AppendLine(line);
+                }
+            }
+            var inputString = inputStringBuilder.ToString();
+            var inputStringReader = new StringReader(inputString);
 
-			var deserializer = new DeserializerBuilder()
-				.IgnoreUnmatchedProperties()
-				.Build();
-			var yamlObject = deserializer.Deserialize(inputStringReader);
-			var serializer = new SerializerBuilder()
-				.JsonCompatible()
-				.Build();
+            var deserializer = new DeserializerBuilder()
+                .IgnoreUnmatchedProperties()
+                .Build();
+            var yamlObject = deserializer.Deserialize(inputStringReader);
+            var serializer = new SerializerBuilder()
+                .JsonCompatible()
+                .Build();
 
-			var json = serializer.Serialize(yamlObject);
-			return JsonConvert.DeserializeObject<ExpandoObject>(json, new ExpandoObjectConverter());
-		}
-	}
+            var json = serializer.Serialize(yamlObject);
+            return JsonConvert.DeserializeObject<ExpandoObject>(json, new ExpandoObjectConverter());
+        }
+    }
 }
